d99f6bdec5970f3127c569e721458ff5ed71128d
Jose David Ballesteros <josebp0107@gmail.com>
2021-10-18 12:17:30 -0500

    Se agrega scrip principal con rutas raiz y login

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import db
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method=='POST':
+        nombre = request.form['nombre']
+        usuario = request.form['usuario']
+        correo = request.form['correo']
+        clave = request.form['clave']
+        db.registrar_usuario(nombre, usuario, correo, clave)
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+    
