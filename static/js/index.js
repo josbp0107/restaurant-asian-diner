763f4f6924274131b2c6e55635f0c5f5953e8e29
acih0435 <iadrian@uninorte.edu.co>
2021-10-20 21:27:07 -0500

    edit

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function(){
     new Glider(document.querySelector(".elementos_lista"), {
          // Mobile-first defaults
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         scrollLock: true,
         dots: '.indicadores',
         arrows: {
