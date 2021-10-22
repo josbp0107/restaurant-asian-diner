b1b93b04a55999e048e22d594e204f616340dbb2
acih0435 <iadrian@uninorte.edu.co>
2021-10-21 22:58:52 -0500

    Funcionalidad register, login, logout.

--- db.py
+++ db.py
@@ -1,12 +1,31 @@
 import sqlite3
-import hashlib
-
+from flask import Flask, render_template, url_for, redirect, request,session
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def registrar_usuario(nombre, usuario, correo, clave, rol):
     with sqlite3.connect("restaurante.db") as con:
         cur = con.cursor()
-        cur.execute('INSERT INTO usuarios(id,nombre,correo,usuario,clave,rol) VALUES (null,?,?,?,?,?)', (nombre, correo, usuario, hashlib.sha1(clave.encode()).hexdigest(),rol)) 
+        cur.execute('INSERT INTO usuarios(nombre,correo,usuario,clave,rol) VALUES (?,?,?,?,?)', (nombre, correo, usuario,clave,rol)) 
         con.commit()
+        
+def wthigo(usuario,clave):
+    with sqlite3.connect("restaurante.db") as con:
+        cur = con.cursor()
+        cur.execute(f'SELECT * FROM usuarios WHERE nombre = "{usuario}"')
+        usuarios = cur.fetchall() 
+        print(usuarios)
+        if len(usuarios) > 0:
+            contraseñaHash = usuarios[0][4]
+            if check_password_hash(contraseñaHash, clave):
+                session.clear()
+                session['id'] = usuarios[0][0]
+                session['nombre'] = usuarios[0][1]
+                session['correo'] = usuarios[0][2]
+                session['clave'] = contraseñaHash
+                session['usuario'] = usuarios[0][3]
+                session['rol'] = usuarios[0][5]
+                return redirect(url_for('index'))  
 
 def check_password(hashed_clave, usuario_clave):
     return hashed_clave == hashlib.sha1(usuario_clave.encode()).hexdigest()
