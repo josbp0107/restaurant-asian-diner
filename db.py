eef847aaa039b97409ae94e9dec1b62fc33fa0f1
acih0435 <iadrian@uninorte.edu.co>
2021-10-28 20:09:18 -0500

    edicion funcional

--- db.py
+++ db.py
@@ -8,26 +8,7 @@ def registrar_usuario(nombre, usuario, correo, clave, rol):
         cur = con.cursor()
         cur.execute('INSERT INTO usuarios(nombre,correo,usuario,clave,rol) VALUES (?,?,?,?,?)', (nombre, correo, usuario,clave,rol)) 
         con.commit()
-        
-def wthigo(usuario,clave):
-    with sqlite3.connect("restaurante.db") as con:
-        cur = con.cursor()
-        cur.execute(f'SELECT * FROM usuarios WHERE usuario = "{usuario}"')
-        usuarios = cur.fetchall() 
-        print(len(usuarios))
-        print(usuarios)
-        if len(usuarios) != 0:
-            contraseñaHash = usuarios[0][4]
-            if check_password_hash(contraseñaHash, clave):
-                session.clear()
-                session['id'] = usuarios[0][0]
-                session['nombre'] = usuarios[0][1]
-                session['correo'] = usuarios[0][2]
-                session['clave'] = contraseñaHash
-                session['usuario'] = usuarios[0][3]
-                session['rol'] = usuarios[0][5]
-            return True
-    
+
 
 
 
